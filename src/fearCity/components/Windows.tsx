4dedd303bfc8f3af8561d04d9e6960de052803c7
elaine <kim85326@gmail.com>
2019-04-02 00:02:24 +0800

    新增材質版恐懼城頁面

--- src/fearCity/components/Windows.tsx
+++ src/fearCity/components/Windows.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Lottie from "react-lottie";
+import * as windowsJson from "../static/windows.json";
 
 class Windows extends React.Component {
   constructor(props: {}) {
@@ -6,74 +8,16 @@ class Windows extends React.Component {
   }
 
   public render() {
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: windowsJson
+    };
+
     return (
-      <svg
-        className="windows"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        x="0px"
-        y="0px"
-        viewBox="0 0 1920 1080"
-      >
-        <g id="背景">
-          <g id="中藍背景_1_">
-            <rect id="中藍背景" fill="#3D6B97" width="1918.9" height="612" />
-          </g>
-          <g id="窗1_1_">
-            <rect
-              id="窗1"
-              x="656.3"
-              y="113.4"
-              fill="#243B61"
-              width="41.2"
-              height="83.4"
-            />
-          </g>
-          <g id="窗1_x5F_1_1_">
-            <rect
-              id="窗1_x5F_1"
-              x="697.6"
-              y="113.4"
-              fill="#2E4874"
-              width="11.5"
-              height="83.4"
-            />
-          </g>
-          <g id="窗2_1_">
-            <rect
-              id="窗2"
-              x="759.8"
-              y="78.7"
-              fill="#FFE381"
-              width="26.5"
-              height="78.6"
-            />
-          </g>
-          <g id="窗2_x5F_2_1_">
-            <rect
-              id="窗2_x5F_2"
-              x="786.3"
-              y="78.7"
-              fill="#2E4874"
-              width="16.5"
-              height="78.6"
-            />
-          </g>
-          <g id="窗3">
-            <rect x="852" y="54.4" fill="#243B61" width="18.7" height="74.9" />
-          </g>
-          <g id="窗3_x5F_3_1_">
-            <rect
-              id="窗3_x5F_3"
-              x="870.7"
-              y="54.4"
-              fill="#2E4874"
-              width="18.6"
-              height="74.9"
-            />
-          </g>
-        </g>
-      </svg>
+      <div className="windows">
+        <Lottie options={defaultOptions} />
+      </div>
     );
   }
 }
