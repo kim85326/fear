20006cc6194e2cb191bf0041a28a05a6a8a29a23
elaine <kim85326@gmail.com>
2019-04-10 23:27:33 +0800

    修改對話框 GHI 動態

--- src/storyA/components/DialogG.tsx
+++ src/storyA/components/DialogG.tsx
@@ -1,100 +1,59 @@
 import * as React from "react";
 import DialogGText from "./DialogGText";
+import { CSSTransition } from "react-transition-group";
 
 interface IDialogGProps {
   isShowDialog: boolean;
 }
 
-interface IDialogGState {
-  isShowDialog: boolean;
-  isShowingDialogBackground: boolean;
-  isHidingDialogBackground: boolean;
-  isShowDialogText: boolean;
-}
-
-class DialogG extends React.Component<IDialogGProps, IDialogGState> {
+class DialogG extends React.Component<IDialogGProps> {
   constructor(props: IDialogGProps) {
     super(props);
-
-    this.state = {
-      isShowDialog: false,
-      isShowingDialogBackground: false,
-      isHidingDialogBackground: false,
-      isShowDialogText: false
-    };
-  }
-
-  public componentWillReceiveProps(nextProps: IDialogGProps) {
-    // 當 props 改變，且顯示對話框時
-    if (nextProps.isShowDialog && !this.props.isShowDialog) {
-      // 顯示對話框背景
-      this.showDialogBackground();
-
-      // 0.5 秒後顯示對話框的文字
-      setTimeout(() => {
-        this.setState({ isShowDialogText: true });
-      }, 500);
-    }
-
-    // 當 props 改變，且不顯示對話框時
-    if (!nextProps.isShowDialog && this.props.isShowDialog) {
-      // 不顯示對話框背景
-      this.hideDialogBackground();
-
-      // 0.5 秒後不顯示對話框的文字
-      setTimeout(() => {
-        this.setState({ isShowDialogText: false });
-      }, 500);
-    }
   }
 
   public render() {
     return (
-      <div className={`dialogG ${this.state.isShowDialog ? "show" : ""}`}>
-        <DialogGText isShowDialogText={this.state.isShowDialogText} />
-        <svg
-          className={`dialogG-background
-            ${this.state.isShowingDialogBackground ? "isShowing" : ""}
-            ${this.state.isHidingDialogBackground ? "isHiding" : ""}`}
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 344.176 294.544"
-        >
-          <path
-            style={{ fill: "#FCFAEE" }}
-            d="M343.393,123.124c-4.16-20.387-23.922-34.965-46.801-36.3c9.506-17.642,4.645-39.253-13.434-52.359
+      <CSSTransition
+        in={this.props.isShowDialog}
+        timeout={{ enter: 1000, exit: 1000 }}
+        classNames="show"
+      >
+        <div className="dialogG dialogGHI">
+          <CSSTransition
+            in={this.props.isShowDialog}
+            timeout={{ enter: 1000, exit: 1000 }}
+            classNames="zoom"
+          >
+            <svg
+              className="dialogG-background dialogGHI-background"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 344.176 294.544"
+            >
+              <path
+                style={{ fill: "#FCFAEE" }}
+                d="M343.393,123.124c-4.16-20.387-23.922-34.965-46.801-36.3c9.506-17.642,4.645-39.253-13.434-52.359
 	c-18.355-13.306-44.34-13.499-62.859-1.815C214.455,13.855,194.652,0,171.054,0c-23.96,0-44.009,14.283-49.505,33.518
 	C103.09,22.366,77.614,22.75,59.535,35.855C41.179,49.162,36.449,71.236,46.55,89.023c-22.438,1.708-41.669,16.151-45.768,36.23
 	c-4.161,20.387,8.639,39.925,29.609,47.939c-15.917,13.77-19.904,35.515-8.103,53.171c8.525,12.761,23.319,20.386,39.08,21.756
 	l-20.095,39.719l42.439-21.974c5.032,11.658,15.672,21.421,30.274,26.013c22.515,7.08,47.008-0.418,59.786-16.869
 	c12.933,15.934,37.024,23.103,59.2,16.129c22.517-7.08,35.702-26.426,33.25-46.125c21.762,5.024,45.551-2.864,57.351-20.521
 	c11.98-17.927,7.687-40.068-8.847-53.798C335.138,162.457,347.492,143.203,343.393,123.124"
-          />
-        </svg>
-      </div>
+              />
+            </svg>
+          </CSSTransition>
+          <CSSTransition
+            in={this.props.isShowDialog}
+            timeout={{ enter: 1000, exit: 1000 }}
+            classNames="show"
+          >
+            <DialogGText />
+          </CSSTransition>
+        </div>
+      </CSSTransition>
     );
-  }
-
-  private showDialogBackground() {
-    this.setState({
-      isShowDialog: true,
-      isShowingDialogBackground: true
-    });
-    setTimeout(() => {
-      this.setState({ isShowingDialogBackground: false });
-    }, 1000);
-  }
-
-  private hideDialogBackground() {
-    this.setState({ isHidingDialogBackground: true });
-    setTimeout(() => {
-      this.setState({
-        isHidingDialogBackground: false,
-        isShowDialog: false
-      });
-    }, 800);
   }
 }
 
--- src/storyA/components/DialogH.tsx
+++ src/storyA/components/DialogH.tsx
@@ -1,96 +1,55 @@
 import * as React from "react";
 import DialogHText from "./DialogHText";
+import { CSSTransition } from "react-transition-group";
 
 interface IDialogHProps {
   isShowDialog: boolean;
 }
 
-interface IDialogHState {
-  isShowDialog: boolean;
-  isShowingDialogBackground: boolean;
-  isHidingDialogBackground: boolean;
-  isShowDialogText: boolean;
-}
-
-class DialogH extends React.Component<IDialogHProps, IDialogHState> {
+class DialogH extends React.Component<IDialogHProps> {
   constructor(props: IDialogHProps) {
     super(props);
-
-    this.state = {
-      isShowDialog: false,
-      isShowingDialogBackground: false,
-      isHidingDialogBackground: false,
-      isShowDialogText: false
-    };
-  }
-
-  public componentWillReceiveProps(nextProps: IDialogHProps) {
-    // 當 props 改變，且顯示對話框時
-    if (nextProps.isShowDialog && !this.props.isShowDialog) {
-      // 顯示對話框背景
-      this.showDialogBackground();
-
-      // 0.5 秒後顯示對話框的文字
-      setTimeout(() => {
-        this.setState({ isShowDialogText: true });
-      }, 500);
-    }
-
-    // 當 props 改變，且不顯示對話框時
-    if (!nextProps.isShowDialog && this.props.isShowDialog) {
-      // 不顯示對話框背景
-      this.hideDialogBackground();
-
-      // 0.5 秒後不顯示對話框的文字
-      setTimeout(() => {
-        this.setState({ isShowDialogText: false });
-      }, 500);
-    }
   }
 
   public render() {
     return (
-      <div className={`dialogH ${this.state.isShowDialog ? "show" : ""}`}>
-        <DialogHText isShowDialogText={this.state.isShowDialogText} />
-        <svg
-          className={`dialogH-background
-            ${this.state.isShowingDialogBackground ? "isShowing" : ""}
-            ${this.state.isHidingDialogBackground ? "isHiding" : ""}`}
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 325.99 247.411"
-        >
-          <path
-            style={{ fill: "#E77452" }}
-            d="M162.995,0c90.02,0,162.996,55.385,162.996,123.706c0,31.491-15.531,60.217-41.063,82.055
+      <CSSTransition
+        in={this.props.isShowDialog}
+        timeout={{ enter: 1000, exit: 1000 }}
+        classNames="show"
+      >
+        <div className="dialogH dialogGHI">
+          <CSSTransition
+            in={this.props.isShowDialog}
+            timeout={{ enter: 1000, exit: 1000 }}
+            classNames="zoom"
+          >
+            <svg
+              className="dialogH-background dialogGHI-background"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 325.99 247.411"
+            >
+              <path
+                style={{ fill: "#E77452" }}
+                d="M162.995,0c90.02,0,162.996,55.385,162.996,123.706c0,31.491-15.531,60.217-41.063,82.055
 	l30.813,41.65l-55.129-24.631c-27.211,15.468-61.004,24.631-97.617,24.631C72.975,247.411,0,192.026,0,123.706
 	C0,55.385,72.975,0,162.995,0"
-          />
-        </svg>
-      </div>
+              />
+            </svg>
+          </CSSTransition>
+          <CSSTransition
+            in={this.props.isShowDialog}
+            timeout={{ enter: 1000, exit: 1000 }}
+            classNames="show"
+          >
+            <DialogHText />
+          </CSSTransition>
+        </div>
+      </CSSTransition>
     );
-  }
-
-  private showDialogBackground() {
-    this.setState({
-      isShowDialog: true,
-      isShowingDialogBackground: true
-    });
-    setTimeout(() => {
-      this.setState({ isShowingDialogBackground: false });
-    }, 1000);
-  }
-
-  private hideDialogBackground() {
-    this.setState({ isHidingDialogBackground: true });
-    setTimeout(() => {
-      this.setState({
-        isHidingDialogBackground: false,
-        isShowDialog: false
-      });
-    }, 800);
   }
 }
 
